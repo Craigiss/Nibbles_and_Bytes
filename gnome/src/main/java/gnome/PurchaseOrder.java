e5cf843543985ca5294a73348c9883aba1c124b2
ooschpoonoo <up692517@myport.ac.uk>
2016-09-28 13:38:29 +0100

    Primary gnome commit v4

--- gnome/src/main/java/gnome/PurchaseOrder.java
+++ gnome/src/main/java/gnome/PurchaseOrder.java
@@ -1,5 +1,70 @@
 package gnome;
 
+import java.util.Date;
+
+/**
+ * 
+ * @author Nibbles and Bytes Version: 1 Class hold information on purchase
+ *         orders to order stock
+ */
 public class PurchaseOrder {
+	private int id;
+	private Date date;
+	private String status;
+
+	/**
+	 * Default Constructor
+	 */
+	public PurchaseOrder() {
+
+	}
+
+	/**
+	 * 
+	 * @return id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * 
+	 * @param id
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * 
+	 * @return date
+	 */
+	public Date getDate() {
+		return date;
+	}
+
+	/**
+	 * 
+	 * @param date
+	 */
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	/**
+	 * 
+	 * @return status
+	 */
+	public String getStatus() {
+		return status;
+	}
+
+	/**
+	 * 
+	 * @param status
+	 */
+	public void setStatus(String status) {
+		this.status = status;
+	}
 
 }
--- gnome/src/main/java/gnome/Review.java
+++ gnome/src/main/java/gnome/Review.java
@@ -1,5 +1,71 @@
 package gnome;
 
+import java.util.Date;
+
+/**
+ * 
+ * @author Nibbles and Bytes - Jacob Version: 1 Class hold information on
+ *         reviews placed by customers
+ */
 public class Review {
+	private Date date;
+	private String reviewText;
+	private int rating;
+
+	/**
+	 * Default Constructor
+	 */
+
+	public Review() {
+
+	}
+
+	/**
+	 * 
+	 * @return date
+	 */
+	public Date getDate() {
+		return date;
+	}
+
+	/**
+	 * 
+	 * @param date
+	 */
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	/**
+	 * 
+	 * @return reviewText
+	 */
+	public String getReviewText() {
+		return reviewText;
+	}
+
+	/**
+	 * 
+	 * @param reviewText
+	 */
+	public void setReviewText(String reviewText) {
+		this.reviewText = reviewText;
+	}
+
+	/**
+	 * 
+	 * @return rating
+	 */
+	public int getRating() {
+		return rating;
+	}
+
+	/**
+	 * 
+	 * @param rating
+	 */
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
 
 }
