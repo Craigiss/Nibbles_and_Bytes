aab12e9196e1f053fb590e1f0188d4181cdec60e
ooschpoonoo <up692517@myport.ac.uk>
2016-09-28 14:33:14 +0100

    Primary gnome commit v1

--- gnome/src/main/java/gnome/PurchaseOrder.java
+++ gnome/src/main/java/gnome/PurchaseOrder.java
@@ -1,20 +1,45 @@
 package gnome;
 
 import java.util.Date;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
+
+
 
 /**
  * 
  * @author Nibbles and Bytes Version: 1 Class hold information on purchase
  *         orders to order stock
  */
+@Entity 
+@Table (name = "PurchaseOrder")
 public class PurchaseOrder {
+	@Id
+	@Column (name = "id")
+	@GeneratedValue (
+	strategy = GenerationType.IDENTITY	)
 	private int id;
+	
+	@Column (name = "date", nullable = false)
+	@NotNull
 	private Date date;
+	
+	@Column (name = "status", nullable = false, length = 60 )
+	@NotNull 
+	@Size (min = 2, max = 60)
 	private String status;
-
+	
+	@ManyToOne 
+	@JoinColumn(name = "FKSupplierid", nullable = false)
+	private int Supplierid;
+	
 	/**
 	 * Default Constructor
 	 */
+	
 	public PurchaseOrder() {
 
 	}
