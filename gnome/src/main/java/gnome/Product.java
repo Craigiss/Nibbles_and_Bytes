6ec217f3776e427611b755022075b2db29e2327b
aboyce <adam@aboyce.uk>
2016-09-28 13:29:14 +0100

    Added the attributes, with getters and setters to the Product and ProductCategory classes.

--- gnome/src/main/java/gnome/Product.java
+++ gnome/src/main/java/gnome/Product.java
@@ -1,5 +1,86 @@
 package gnome;
 
 public class Product {
+	int productID;
+	String productName;
+	String description;
+	double price;
+	int stockLevel;
+	int supplierID;
+	
+	/**
+	 * @return the productID
+	 */
+	public int getProductID() {
+		return productID;
+	}
+	/**
+	 * @param productID the productID to set
+	 */
+	public void setProductID(int productID) {
+		this.productID = productID;
+	}
+	/**
+	 * @return the productName
+	 */
+	public String getProductName() {
+		return productName;
+	}
+	/**
+	 * @param productName the productName to set
+	 */
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	/**
+	 * @return the price
+	 */
+	public double getPrice() {
+		return price;
+	}
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	/**
+	 * @return the stockLevel
+	 */
+	public int getStockLevel() {
+		return stockLevel;
+	}
+	/**
+	 * @param stockLevel the stockLevel to set
+	 */
+	public void setStockLevel(int stockLevel) {
+		this.stockLevel = stockLevel;
+	}
+	/**
+	 * @return the supplierID
+	 */
+	public int getSupplierID() {
+		return supplierID;
+	}
+	/**
+	 * @param supplierID the supplierID to set
+	 */
+	public void setSupplierID(int supplierID) {
+		this.supplierID = supplierID;
+	}
+	
+	
 
 }
--- gnome/src/main/java/gnome/ProductCategory.java
+++ gnome/src/main/java/gnome/ProductCategory.java
@@ -1,5 +1,30 @@
 package gnome;
 
 public class ProductCategory {
-
+	int categoryID;
+	int productID;
+	/**
+	 * @return the categoryID
+	 */
+	public int getCategoryID() {
+		return categoryID;
+	}
+	/**
+	 * @param categoryID the categoryID to set
+	 */
+	public void setCategoryID(int categoryID) {
+		this.categoryID = categoryID;
+	}
+	/**
+	 * @return the productID
+	 */
+	public int getProductID() {
+		return productID;
+	}
+	/**
+	 * @param productID the productID to set
+	 */
+	public void setProductID(int productID) {
+		this.productID = productID;
+	}
 }
