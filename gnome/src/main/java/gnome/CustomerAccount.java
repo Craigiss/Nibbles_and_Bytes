bc62b8678564ef1215680642d8a1a4699bcee8b7
kb12abz <kjbagnall@icloud.com>
2016-09-28 13:36:15 +0000

    Addition of CustomerAccount.java SalesOrder.java and Returns.java

--- gnome/src/main/java/gnome/CustomerAccount.java
+++ gnome/src/main/java/gnome/CustomerAccount.java
@@ -1,9 +1,113 @@
 package gnome;
 
+/**
+ * @Author Nibbles And Bytes
+ * @version 1.0
+ * Class holds information on a customer sorted within the database
+ * */
+
+
 public class CustomerAccount {
-	private String username;
+	private String email;
+	private String password;
+	private String firstName;
+	private String surname;
+	private String title;
+	private int phoneNumber;
 	
-	public CustomerAccount(){
-		
+	public CustomerAccount(){	
+	}
+
+	/**
+	 * Returns email address of customer
+	 * @return emails address
+	 * */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * Sets email address of customer
+	 * @param String email - email address of customer
+	 * */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * Returns password customer
+	 * @return customer password
+	 * */
+	public String getPassword() {
+		return password;
+	}
+
+	
+	/**
+	 * Sets password of a customer
+	 * @param String password - password for customer
+	 * */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * Sets first name of a customer
+	 * @param String firstname - firstname of customer
+	 * */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * Returns surname of customer
+	 * @return customer surname
+	 * */
+	public String getSurname() {
+		return surname;
+	}
+
+	/**
+	 * Sets surname of a customer
+	 * @param String surname - surname of customer
+	 * */
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	/**
+	 * Returns title of customer
+	 * @return title
+	 * */
+	public String getTitle() {
+		return title;
+	}
+
+	/**
+	 * Sets title  of a customer
+	 * @param String title - title of customer
+	 * */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	/**
+	 * Returns phone number of a customer
+	 * @return phone number
+	 * */
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	/**
+	 * Sets phone number of a customer
+	 * @param String phoneNumber - phone number of a customer
+	 * */
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
 	}
 }
--- gnome/src/main/java/gnome/SalesOrder.java
+++ gnome/src/main/java/gnome/SalesOrder.java
@@ -1,5 +1,84 @@
 package gnome;
+import java.sql.Date;
+
+/**
+ * @Author Nibbles And Bytes
+ * @version 1.0
+ * Class holds information on a sales order sorted within the database
+ * */
 
 public class SalesOrder {
+
+	private int id;
+	private Date date;
+	private CustomerAccount customer;
+	private String status;
+
+	public SalesOrder(){	
+	}
+
+	/**
+	 * Returns order id of a customer order
+	 * @return order id
+	 * */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * Sets order id of a customer order
+	 * @param Int id - email address of customer
+	 * */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * Returns Date that a customer order was placed
+	 * @return Date
+	 * */
+	public Date getDate() {
+		return date;
+	}
+
+	/**
+	 * Sets date of a customer order
+	 * @param String Date - email address of customer
+	 * */
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	/**
+	 * Returns an instance of a customer that placed the order
+	 * @return Customer object
+	 * */
+	public CustomerAccount getCustomer() {
+		return customer;
+	}
+
+	/**
+	 * Assigns a customer object to an order
+	 * @param Customer Object
+	 * */
+	public void setCustomer(CustomerAccount customer) {
+		this.customer = customer;
+	}
+
+	/**
+	 * Returns  status of a customer order
+	 * @return status
+	 * */
+	public String getStatus() {
+		return status;
+	}
+
+	/**
+	 * Sets the status of a customer order
+	 * @param String status
+	 * */
+	public void setStatus(String status) {
+		this.status = status;
+	}
 
 }
