b5fe0ada5bd05be1b6bf94323d3db0660db202e4
Sqidge <connor.sqidge@hotmail.co.uk>
2016-09-28 13:51:11 +0000

    added getters and setters

--- gnome/src/main/java/gnome/CustomerAccountAddress.java
+++ gnome/src/main/java/gnome/CustomerAccountAddress.java
@@ -1,7 +1,44 @@
 package gnome;
 
-public class CustomerAccountAddress {
+import java.util.List;
 
-	//FK username
-	// FK address id
+import javax.persistence.*;
+
+public class CustomerAccountAddress {
+	@OneToMany
+	@JoinColumn(name="username_fk", nullable = false)
+	private CustomerAccount username;
+	
+	@ManyToOne
+	@JoinColumn(name="address_fk", nullable = false)
+	private List<Address> address;
+
+	/**
+	 * @return the username
+	 */
+	public CustomerAccount getUsername() {
+		return username;
+	}
+
+	/**
+	 * @param username the username to set
+	 */
+	public void setUsername(CustomerAccount username) {
+		this.username = username;
+	}
+
+	/**
+	 * @return the address
+	 */
+	public List<Address> getAddress() {
+		return address;
+	}
+
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(List<Address> address) {
+		this.address = address;
+	}
+
 }
