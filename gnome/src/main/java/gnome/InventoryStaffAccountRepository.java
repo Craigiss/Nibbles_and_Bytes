66fab65deb3b34fe3dc9c7e24349e0d295c8dcd0
Craigiss <Craigiss9@googlemail.com>
2016-09-30 12:50:52 +0100

    Inventory Staff Account Repository/Offline Inserted

---
+++ gnome/src/main/java/gnome/InventoryStaffAccountRepository.java
@@ -0,0 +1,23 @@
+package gnome;
+
+import java.util.List;
+
+/**
+ * Interface for InventoryStaffAccount Manager classes
+ * InventoryStaffAccount Managers will have these methods
+ * @author Nibbles and Bytes
+ */
+
+public interface InventoryStaffAccountRepository {
+	
+	//Create
+	public void persistInventoryStaffAccount(InventoryStaffAccount isa);
+	
+	//Read
+	public List<InventoryStaffAccount> findInventoryStaffAccount(InventoryStaffAccount isa);
+	
+	//Update
+	public void updateInventoryStaffAccountName(InventoryStaffAccount isa);
+	public void updateInventoryStaffAccountEmail(InventoryStaffAccount isa);
+	public void updateInventoryStaffAccountPassword(InventoryStaffAccount isa);
+}
