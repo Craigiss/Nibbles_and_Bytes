9dd731f26bfe4df59726caa8715da01420fe711c
Sqidge <connor.sqidge@hotmail.co.uk>
2016-09-30 14:09:02 +0000

    changed initial data

--- gnome/src/main/java/gnome/CategoryRepository.java
+++ gnome/src/main/java/gnome/CategoryRepository.java
@@ -1,5 +1,19 @@
 package gnome;
 
-public class CategoryRepository {
+import java.util.List;
+
+import com.nb.gnome.entities.*;
+
+/**
+ * Interface for CategoryRepository 
+ * 
+ * @author Nibbles and Bytes
+ */
+public interface CategoryRepository {
+	//Create
+	public void persistCategory(Category c);
+	
+	//Read
+	public List<Address> findCategory(Category c);
 
 }
--- gnome/src/main/java/gnome/CustomerAccountAddressRepository.java
+++ gnome/src/main/java/gnome/CustomerAccountAddressRepository.java
@@ -1,5 +1,19 @@
 package gnome;
 
-public class CustomerAccountAddressRepository {
+import java.util.List;
+
+import com.nb.gnome.entities.Address;
+
+/**
+ * Interface for CustomerAccountAddress classes AddressManagers will have these methods
+ * 
+ * @author Nibbles and Bytes
+ */
+public interface CustomerAccountAddressRepository {
+
+	// Create
+	public void persistCustomerAddress(Address a);
+
+
 
 }
