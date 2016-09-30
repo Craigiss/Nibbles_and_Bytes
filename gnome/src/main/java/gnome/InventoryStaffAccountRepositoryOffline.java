3e93075c7d2ecd272f39eca7733df66f807dff7d
kb12abz <kjbagnall@icloud.com>
2016-09-30 13:56:30 +0000

    error removal

--- gnome/src/main/java/gnome/InventoryStaffAccountRepositoryOffline.java
+++ gnome/src/main/java/gnome/InventoryStaffAccountRepositoryOffline.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import com.nb.gnome.entities.InventoryStaffAccount;
+
 public class InventoryStaffAccountRepositoryOffline implements InventoryStaffAccountRepository{
 	
 	@Inject
@@ -12,6 +14,31 @@ public class InventoryStaffAccountRepositoryOffline implements InventoryStaffAcc
 	//@Override - this should work, but doesn't so it has been commented out
 	public void persistInventoryStaffAccount(InventoryStaffAccount isa) {
 		initialData.addInventoryStaffAccount(isa);		
+	}
+
+	public List<InventoryStaffAccount> findInventoryStaffAccount(InventoryStaffAccount isa) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void updateInventoryStaffAccountName(InventoryStaffAccount isa) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void updateInventoryStaffAccountEmail(InventoryStaffAccount isa) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void updateInventoryStaffAccountPassword(InventoryStaffAccount isa) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void persistInventoryStaffAccount(InventoryStaffAccount isa) {
+		// TODO Auto-generated method stub
+		
 	}
 
 	public List<InventoryStaffAccount> findInventoryStaffAccount(InventoryStaffAccount isa) {
