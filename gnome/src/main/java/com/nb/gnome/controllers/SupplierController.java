5cfe731549eb005b38f75c048b253619d5644c40
beejorama <brdb92@gmail.com>
2016-10-13 15:14:03 +0100

    Checking git

--- gnome/src/main/java/com/nb/gnome/controllers/SupplierController.java
+++ gnome/src/main/java/com/nb/gnome/controllers/SupplierController.java
@@ -1,5 +1,63 @@
 package com.nb.gnome.controllers;
 
-public class SupplierController {
+import java.io.Serializable;
+import com.nb.gnome.entities.Supplier;
+import com.nb.gnome.helper.PaginationHelper;
+import gnome.SupplierRepository;
 
-}
+
+import javax.enterprise.context.SessionScoped;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named("suppliers")
+@SessionScoped
+public class SupplierController implements Serializable{
+	@Inject
+	private SupplierRepository supplierRepository;
+	private  Supplier supplier;
+	private PaginationHelper pagination;
+	private int selected;
+	private DataModel<Supplier> dataModel = null;
+
+	private void recreateModel() {
+		dataModel = null;
+	}
+	
+	public PaginationHelper getPagination() {
+		if (pagination == null)	pagination = new PaginationHelper(10){
+		@Override public int getItemsCount() {
+			return supplierRepository.findAll().size();
+		}
+	
+		@Override public DataModel<Supplier>createPageDataModel(){
+			try{
+				return new ListDataModel<Supplier>(supplierRepository.findAll().subList(getPageFirstItem(),getPageFirstItem() + getPageSize()));
+			} catch (Exception e) { return new ListDataModel<Supplier>(supplierRepository.findAll().subList(getPageFirstItem(),getItemsCount()));
+		}
+		}
+		};
+		return pagination;
+	}
+	
+	public DataModel<Supplier> getDataModel() {
+		if (dataModel ==null)
+			dataModel = getPagination().createPageDataModel();
+		System.out.println("Delete this after checking GIT works");
+		System.out.println("Delete this after checking GIT works");
+		System.out.println("Delete this after checking GIT works");
+		System.out.println("Delete this after checking GIT works");
+		System.out.println("Delete this after checking GIT works");
+		System.out.println("Delete this after checking GIT works");
+		System.out.println("Delete this after checking GIT works");
+		return dataModel;
+		
+	}
+	
+	// Delete this after checking GIT works
+	
+	
+	}
+
