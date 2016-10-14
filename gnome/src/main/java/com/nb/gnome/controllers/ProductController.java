20dc32f0dc69299a337008d30163ecae5d1a7e2e
kb12abz <kjbagnall@icloud.com>
2016-10-14 12:02:49 +0100

    changes made to scope

--- gnome/src/main/java/com/nb/gnome/controllers/ProductController.java
+++ gnome/src/main/java/com/nb/gnome/controllers/ProductController.java
@@ -27,7 +27,7 @@ public class ProductController implements Serializable {
 
 	public PaginationHelper getPagination() {
 		if (pagination == null)
-			pagination = new PaginationHelper(1) {
+			pagination = new PaginationHelper(10) {
 				@Override
 				public int getItemsCount() {
 					return productRepository.findAll().size();
