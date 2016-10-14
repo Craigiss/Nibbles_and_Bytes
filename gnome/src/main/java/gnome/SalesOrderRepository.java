9418e734d1bc691d94278166616f7957e119fb31
ooschpoonoo <up692517@myport.ac.uk>
2016-10-14 11:38:16 +0100

    finished both sales repositories

--- gnome/src/main/java/gnome/SalesOrderRepository.java
+++ gnome/src/main/java/gnome/SalesOrderRepository.java
@@ -1,5 +1,16 @@
 package gnome;
 
-public interface SalesOrderRepository {
+import java.util.List;
 
+import com.nb.gnome.entities.SalesOrder;
+
+public interface SalesOrderRepository {
+	//Create
+	public void persistSalesOrder(SalesOrder a);
+	//Read
+	public List<SalesOrder> findSalesOrder();
+	
+	public SalesOrder findSalesOrderById(long id);
+	
+	public List<SalesOrder> findAll();
 }
