36d11d4c93755f8c265e150fa84b346abea91606
ooschpoonoo <up692517@myport.ac.uk>
2016-10-14 12:52:07 +0100

    complete salesorder controller

--- gnome/src/main/java/com/nb/gnome/controllers/OrderController.java
+++ gnome/src/main/java/com/nb/gnome/controllers/OrderController.java
@@ -26,18 +26,50 @@ public class OrderController implements Serializable {
 		dataModel = null; 
 	}
 	
-//	public PaginationHelper getPagination(){
-//		if (pagination == null) 
-//			pagination = new PaginationHelper(10){
-//			@Override public int getItemsCount(){
-//				return salesOrderManager.findAll().size();
-//			}
-//			
-//			@Override public DataModel<SalesOrder>createPageDataModel(){
-//				try{ 
-//					return new ListDataModel<SalesOrder>(salesOrderManager.findAll().subList(getPageFirstItem(),getPageFirstItem() + getPageSize()));
-//				} catch (Exception e){ return new ListDataModel<SalesOrder>(salesOrderManager.findAll().subList(getPageFirstItem(),getItemsCount()));
-//			}
-//		}
-//	}
+	public PaginationHelper getPagination(){
+		if (pagination == null)	pagination = new PaginationHelper(10){
+			@Override public int getItemsCount(){
+				return salesOrderManager.findAll().size();
+			}
+			
+			@Override public DataModel<SalesOrder>createPageDataModel(){
+				try{ 
+					return new ListDataModel<SalesOrder>(salesOrderManager.findAll().subList(getPageFirstItem(),getPageFirstItem() + getPageSize()));
+				} catch (Exception e){ return new ListDataModel<SalesOrder>(salesOrderManager.findAll().subList(getPageFirstItem(),getItemsCount()));
+			}		
+			}
+			};
+			return pagination; 
+	}
+
+	public DataModel<SalesOrder> getDataModel(){
+		if (dataModel == null)
+			dataModel = getPagination().createPageDataModel();
+		return dataModel;
+		
+	}
+	
+	public String next(){
+		getPagination().nextPage();
+		recreateModel();
+		return "salesOrder";
+	}
+	
+	public String previous(){
+		getPagination().previousPage();
+		recreateModel();
+		return "salesOrder";
+	}
+	
+	public SalesOrder view (long id){
+		salesOrder = salesOrderManager.findSalesOrderById(id);
+		return salesOrder;
+	}
+	public SalesOrder getSalesOrder (){
+		return salesOrder;
+	}
+	public void setSalesOrder (SalesOrder salesOrder){
+		this.salesOrder = salesOrder;
+	}
 }
+
