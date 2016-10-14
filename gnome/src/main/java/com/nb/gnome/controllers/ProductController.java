4771584cefad5cc6a51eb36425ab03e6dd948b20
kb12abz <kjbagnall@icloud.com>
2016-10-14 09:25:40 +0100

    stuff happened

--- gnome/src/main/java/com/nb/gnome/controllers/ProductController.java
+++ gnome/src/main/java/com/nb/gnome/controllers/ProductController.java
@@ -1,98 +1,98 @@
-//package com.nb.gnome.controllers;
-//
-//import java.io.Serializable;
-//import javax.enterprise.context.SessionScoped;
-//import javax.faces.model.DataModel;
-//import javax.faces.model.ListDataModel;
-//import javax.inject.Inject;
-//import javax.inject.Named;
-//import com.nb.gnome.helper.*;
-//import com.nb.gnome.entities.Product;
-//
-//import gnome.ProductRepository;
-//
-//@Named("products")
-//@SessionScoped
-//public class ProductController implements Serializable {
-//	@Inject
-//	private ProductRepository prodManager;
-//	private Product product;
-//	private PaginationHelper pagination;
-//	private int selected;
-//	private DataModel<Product> dataModel = null;
-//
-//	private void recreateModel() {
-//		dataModel = null;
-//	}
-//
-//	public PaginationHelper getPagination() {
-//		if (pagination == null)
-//			pagination = new PaginationHelper(10) {
-//				@Override
-//				public int getItemsCount() {
-//					return prodManager.findAll().size();
-//				}
-//
-//				@Override
-//				public DataModel<Product> createPageDataModel() {
-//					try {
-//						return new ListDataModel<Product>(
-//								prodManager.findAll().subList(getPageFirstItem(), getPageFirstItem() + getPageSize()));
-//					} catch (Exception e) {
-//						return new ListDataModel<Product>(
-//								prodManager.findAll().subList(getPageFirstItem(), getItemsCount()));
-//					}
-//				}
-//
-//			};
-//
-//		return pagination;
-//	}
-//
-//	public DataModel<Product> getDataModel() {
-//		if (dataModel == null)
-//			dataModel = getPagination().createPageDataModel();
-//		return dataModel;
-//	}
-//
-//	private void updateCurrentItem() {
-//		int count = prodManager.findAll().size();
-//		if (selected >= count) {
-//			selected = count - 1;
-//			if (pagination.getPageFirstItem() >= count)
-//				pagination.previousPage();
-//		}
-//		if (selected >= 0)
-//			try {
-//				setProduct(prodManager.findAll().subList(selected, selected + 1).get(0));
-//			} catch (Exception e) {
-//				setProduct(prodManager.findAll().subList(selected, count).get(0));
-//			}
-//	}
-//	
-//	public String next(){
-//		getPagination().nextPage();
-//		recreateModel();
-//		return "browse";
-//	}
-//	
-//	public String previous(){
-//		getPagination().previousPage();
-//		recreateModel();
-//		return "browse";
-//	}
-//	
-//	public String view (int id){
-//		product = prodManager.getProductByID(id);
-//		return "product";
-//	}
-//	
-//	public Product getProduct(){
-//		return product;
-//	}
-//	
-//	public void setProduct(Product product){
-//		this.product = product; 
-//	}
-//
-//}
+package com.nb.gnome.controllers;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
+import javax.inject.Inject;
+import javax.inject.Named;
+import com.nb.gnome.helper.*;
+import com.nb.gnome.entities.Product;
+
+import gnome.ProductRepository;
+
+@Named("products")
+@SessionScoped
+public class ProductController implements Serializable {
+	@Inject
+	private ProductRepository prodManager;
+	private Product product;
+	private PaginationHelper pagination;
+	private int selected;
+	private DataModel<Product> dataModel = null;
+
+	private void recreateModel() {
+		dataModel = null;
+	}
+
+	public PaginationHelper getPagination() {
+		if (pagination == null)
+			pagination = new PaginationHelper(10) {
+				@Override
+				public int getItemsCount() {
+					return prodManager.findAll().size();
+				}
+
+				@Override
+				public DataModel<Product> createPageDataModel() {
+					try {
+						return new ListDataModel<Product>(
+								prodManager.findAll().subList(getPageFirstItem(), getPageFirstItem() + getPageSize()));
+					} catch (Exception e) {
+						return new ListDataModel<Product>(
+								prodManager.findAll().subList(getPageFirstItem(), getItemsCount()));
+					}
+				}
+
+			};
+
+		return pagination;
+	}
+
+	public DataModel<Product> getDataModel() {
+		if (dataModel == null)
+			dataModel = getPagination().createPageDataModel();
+		return dataModel;
+	}
+
+	private void updateCurrentItem() {
+		int count = prodManager.findAll().size();
+		if (selected >= count) {
+			selected = count - 1;
+			if (pagination.getPageFirstItem() >= count)
+				pagination.previousPage();
+		}
+		if (selected >= 0)
+			try {
+				setProduct(prodManager.findAll().subList(selected, selected + 1).get(0));
+			} catch (Exception e) {
+				setProduct(prodManager.findAll().subList(selected, count).get(0));
+			}
+	}
+	
+	public String next(){
+		getPagination().nextPage();
+		recreateModel();
+		return "browse";
+	}
+	
+	public String previous(){
+		getPagination().previousPage();
+		recreateModel();
+		return "browse";
+	}
+	
+	public String view (int id){
+		product = prodManager.getProductByID(id);
+		return "product";
+	}
+	
+	public Product getProduct(){
+		return product;
+	}
+	
+	public void setProduct(Product product){
+		this.product = product; 
+	}
+
+}
--- gnome/src/main/java/com/nb/gnome/controllers/PurchaseOrderController.java
+++ gnome/src/main/java/com/nb/gnome/controllers/PurchaseOrderController.java
@@ -1,77 +1,77 @@
-//package com.nb.gnome.controllers;
-//
-//import java.io.Serializable;
-//import com.nb.gnome.entities.PurchaseOrder;
-//import com.nb.gnome.helper.PaginationHelper;
-//import gnome.PurchaseOrderRepository;
-//
-//
-//import javax.enterprise.context.SessionScoped;
-//import javax.faces.model.DataModel;
-//import javax.faces.model.ListDataModel;
-//import javax.inject.Inject;
-//import javax.inject.Named;
-//
-//@Named("pos")
-//@SessionScoped
-//public class PurchaseOrderController implements Serializable{
-//	@Inject
-//	private PurchaseOrderRepository purchaseOrderRepository;
-//	private PurchaseOrder purchaseOrder;
-//	private PaginationHelper pagination;
-//	private int selected;
-//	private DataModel<PurchaseOrder> dataModel = null;
-//
-//	private void recreateModel() {
-//		dataModel = null;
-//	}
-//	
-//	public PaginationHelper getPagination() {
-//		if (pagination == null)	pagination = new PaginationHelper(10){
-//		@Override public int getItemsCount() {
-//			return purchaseOrderRepository.findAll().size();
-//		}
-//	
-//		@Override public DataModel<PurchaseOrder>createPageDataModel(){
-//			try{
-//				return new ListDataModel<PurchaseOrder>(purchaseOrderRepository.findAll().subList(getPageFirstItem(),getPageFirstItem() + getPageSize()));
-//			} catch (Exception e) { return new ListDataModel<PurchaseOrder>(purchaseOrderRepository.findAll().subList(getPageFirstItem(),getItemsCount()));
-//		}
-//		}
-//		};
-//		return pagination;
-//	}
-//	
-//	public DataModel<PurchaseOrder> getDataModel() {
-//		if (dataModel ==null)
-//			dataModel = getPagination().createPageDataModel();
-//		return dataModel;
-//		
-//	}
-//	
-//	public String next() {
-//		getPagination().nextPage();
-//		recreateModel();
-//		return "imsPo";
-//	}
-//	
-//	public String previous() {
-//		getPagination().previousPage();
-//		recreateModel();
-//		return "imsPo";
-//	}
-//	
-//	public PurchaseOrder view(long id) {
-//		purchaseOrder = purchaseOrderRepository.findPurchaseOrderById(id);
-//		return purchaseOrder;
-//	}
-//	
-//	public PurchaseOrder getPurchaseOrder() {
-//		return purchaseOrder;
-//	}
-//	
-//	public void setPurchaseOrder(PurchaseOrder purchaseOrder) {
-//		this.purchaseOrder = purchaseOrder;
-//	}	
-//	}
-//
+package com.nb.gnome.controllers;
+
+import java.io.Serializable;
+import com.nb.gnome.entities.PurchaseOrder;
+import com.nb.gnome.helper.PaginationHelper;
+import gnome.PurchaseOrderRepository;
+
+
+import javax.enterprise.context.SessionScoped;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named("pos")
+@SessionScoped
+public class PurchaseOrderController implements Serializable{
+	@Inject
+	private PurchaseOrderRepository purchaseOrderRepository;
+	private PurchaseOrder purchaseOrder;
+	private PaginationHelper pagination;
+	private int selected;
+	private DataModel<PurchaseOrder> dataModel = null;
+
+	private void recreateModel() {
+		dataModel = null;
+	}
+	
+	public PaginationHelper getPagination() {
+		if (pagination == null)	pagination = new PaginationHelper(10){
+		@Override public int getItemsCount() {
+			return purchaseOrderRepository.findAll().size();
+		}
+	
+		@Override public DataModel<PurchaseOrder>createPageDataModel(){
+			try{
+				return new ListDataModel<PurchaseOrder>(purchaseOrderRepository.findAll().subList(getPageFirstItem(),getPageFirstItem() + getPageSize()));
+			} catch (Exception e) { return new ListDataModel<PurchaseOrder>(purchaseOrderRepository.findAll().subList(getPageFirstItem(),getItemsCount()));
+		}
+		}
+		};
+		return pagination;
+	}
+	
+	public DataModel<PurchaseOrder> getDataModel() {
+		if (dataModel ==null)
+			dataModel = getPagination().createPageDataModel();
+		return dataModel;
+		
+	}
+	
+	public String next() {
+		getPagination().nextPage();
+		recreateModel();
+		return "imsPo";
+	}
+	
+	public String previous() {
+		getPagination().previousPage();
+		recreateModel();
+		return "imsPo";
+	}
+	
+	public PurchaseOrder view(long id) {
+		purchaseOrder = purchaseOrderRepository.findPurchaseOrderById(id);
+		return purchaseOrder;
+	}
+	
+	public PurchaseOrder getPurchaseOrder() {
+		return purchaseOrder;
+	}
+	
+	public void setPurchaseOrder(PurchaseOrder purchaseOrder) {
+		this.purchaseOrder = purchaseOrder;
+	}	
+	}
+
