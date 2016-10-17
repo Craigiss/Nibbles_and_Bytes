4095270a11d2f172f184502ae8f880acda144197
beejorama <brdb92@gmail.com>
2016-10-17 16:57:27 +0100

    find all supplier details implemented in supplier service

--- gnome/src/main/java/com/nb/gnome/service/SupplierService.java
+++ gnome/src/main/java/com/nb/gnome/service/SupplierService.java
@@ -1,5 +1,7 @@
 package com.nb.gnome.service;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
@@ -13,8 +15,22 @@ public class SupplierService {
 	@Inject
 	private SupplierRepository supplierManager;
 	
+	/**
+	 *  'persists' (adds) a new supplier to the system
+	 * @param supsup
+	 */
 	public void persistSupplier(Supplier supsup){
 		supplierManager.persistSupplier(supsup);
+	}
+
+	/**
+	 *  finds all suppliers and returns these deets as a list
+	 * @return
+	 */
+	public List<Supplier> findAll(){
+		List<Supplier> s = supplierManager.findAll();
+		return s;
+		
 	}
 
 }
