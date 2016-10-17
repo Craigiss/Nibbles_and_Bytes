b2ce0e122e502bb5788fdffef73df75f2f43d543
cameronsmart <camcars@hotmail.co.uk>
2016-10-17 16:40:51 +0100

    CreateAccountController setID  changed.

--- gnome/src/main/java/com/nb/gnome/service/CreateAccountService.java
+++ gnome/src/main/java/com/nb/gnome/service/CreateAccountService.java
@@ -9,13 +9,17 @@ import javax.inject.Inject;
 
 import com.nb.gnome.entities.Customer;
 import com.nb.gnome.managers.CustomerRepository;
+
+import gnome.InitialData;
+
 import com.nb.gnome.entities.Address;
 
 @Stateless
 public class CreateAccountService {
 	@Inject 
 	CustomerRepository customerManager; 
-	
+	@Inject 
+	InitialData initialData;
 	public void newUser(String firstName, String surname, String email, String firstLine, String postcode, String password){
 		//Hashing and stuff, split the name to first and last 
 		
@@ -31,7 +35,7 @@ public class CreateAccountService {
 		c.setFirstName(firstName);					// Does the customer credentials.
 		c.setSurname(surname);
 		c.setEmail(email);
-		c.setId(001);
+		c.setId(initialData.getCustomerAccounts().size() + 1);
 		c.setStatus("active");
 		c.setPassword(password);
 
